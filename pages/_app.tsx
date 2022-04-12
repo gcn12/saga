e00715f5f4f2238d314eea6562d7dfa75de21934
Gareth Ng <garethcng@gmail.com>
2022-04-12 09:59:05 -0400

    Installed fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,15 @@ import { GlobalStyles } from "../styles/GlobalStyles";
 import { Toaster } from "react-hot-toast";
 import "@reach/dialog/styles.css";
 import { SessionProvider } from "next-auth/react";
+import "@fontsource/poppins/100.css";
+import "@fontsource/poppins/200.css";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+import "@fontsource/poppins/800.css";
+import "@fontsource/poppins/900.css";
 
 export default function App({
   Component,
