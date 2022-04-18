27c99426083290a8b0e5f65ed49f97147b51ae79
Gareth Ng <garethcng@gmail.com>
2022-04-18 17:32:17 -0400

    Added cypress testing library

--- cypress/integration/signup.js
+++ cypress/integration/signup.js
@@ -1,22 +1,22 @@
 describe("User can sign up", () => {
   it("User auth works", () => {
     cy.visit("/");
-    cy.contains("Get started").click();
-    cy.contains("label", "Name").type("Daniel Smith");
-    cy.contains("label", "Username").type("danny");
-    cy.contains("button", "CREATE").click();
+    cy.findByText(/get started/i).click();
+    cy.findByLabelText("Name").type("Daniel Smith");
+    cy.findByLabelText("Username").type("danny");
+    cy.findByText(/create/i).click();
   });
 
   it("Create profile works", () => {
     cy.contains("label", "Profile picture url").type(
       "https://images.unsplash.com/photo-1522069169874-c58ec4b76be5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGFuaW1hbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
     );
-    cy.contains("label", "Location").type("Redwood City, CA");
-    cy.contains("label", "Career").type("Analyst at Grammarly");
-    cy.contains("label", "Bio").type(
+    cy.findByLabelText(/location/i).type("Redwood City, CA");
+    cy.findByLabelText(/career/i).type("Analyst at Grammarly");
+    cy.findByLabelText(/bio/i).type(
       "Leo urna molestie at elementum eu facilisis sed."
     );
-    cy.contains("Create user").click();
+    cy.findByText(/create user/i).click();
   });
 
   it("Can delete profile", () => {
